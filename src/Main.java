e688abf676faa93c32761048d0ef8bb4440a4f3c
Maria Loginova <marialog@yandex.ru>
2023-01-30 17:04:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 200;
+        int mille = 20;
+
+        int bonus = price / mille;
+
+        System.out.println( "Бонусные мили за вашу покупку: " + bonus);
+
+
+
+    }
+}
